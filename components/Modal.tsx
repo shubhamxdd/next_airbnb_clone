00ff7b118754b3130ab04ac74409115a39c6e674
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-10-06 23:33:27 +0530

    create footer content on register modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -96,6 +96,7 @@ const Modal = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
