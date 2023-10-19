20ea8578b299994cb6cf915bfb597d8a345fa4e5
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-10-19 19:01:53 +0530

    create CountrySelect component to select country

--- components/RentModal.tsx
+++ components/RentModal.tsx
@@ -7,6 +7,7 @@ import Heading from "./Heading";
 import { categories } from "./Categories";
 import CategoryInput from "./CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "./CountrySelect";
 
 enum STEPS {
   CATEGORY = 0,
@@ -42,6 +43,7 @@ const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -93,13 +95,28 @@ const RentModal = () => {
     </div>
   );
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located?"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+      </div>
+    );
+  }
+
   return (
     <Modal
       title="Airbnb your home"
       body={bodyContent}
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
