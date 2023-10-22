96cba2037984c01817b881436cce7a9ab54f2146
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-10-22 14:00:09 +0530

    Create ListingPage

---
+++ app/listings/[listingId]/page.tsx
@@ -0,0 +1,29 @@
+import getCurrentUser from "@/actions/getCurrentUser";
+import getListingsById from "@/actions/getListingsById";
+import EmptyState from "@/components/EmptyState";
+import ListingClient from "./ListingClient";
+
+interface Props {
+  listingId?: string;
+}
+
+const ListingsPage = async ({ params }: { params: Props }) => {
+  const { listingId } = params;
+  const currentUser = await getCurrentUser();
+
+  const listing = await getListingsById(params);
+
+  if (!listing) {
+    return <EmptyState subtitle="No listing found!" />;
+  }
+
+  return (
+    <>
+      <div>
+        <ListingClient listing={listing} currentUser={currentUser} />
+      </div>
+    </>
+  );
+};
+
+export default ListingsPage;
