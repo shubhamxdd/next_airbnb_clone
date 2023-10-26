db68e1988fb67884b84ef6f4b28fe3ea93ace07f
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-10-27 01:50:49 +0530

    show current search queries in searchbar

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,23 +1,62 @@
 "use client";
 
+import useCountries from "@/hooks/useCountries";
 import useSearchModal from "@/hooks/useSearchModal";
+import { differenceInDays } from "date-fns";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 // import { BiSearch } from "react-icons/bi";
 import { ImSearch } from "react-icons/im";
 
 const Search = () => {
   const searchodal = useSearchModal();
+  const searchParams = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const locationValue = searchParams?.get("locationValue");
+  const startDate = searchParams?.get("startDate");
+  const endDate = searchParams?.get("endDate");
+  const guestCount = searchParams?.get("guestCount");
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) return getByValue(locationValue as string)?.label;
+    return "Search";
+  }, [getByValue, locationValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const startD = new Date(startDate as string);
+      const endD = new Date(endDate as string);
+
+      let diffInDays = differenceInDays(endD, startD);
+
+      if (diffInDays === 0) {
+        diffInDays = 1;
+      }
+
+      return `${diffInDays} ${diffInDays === 1 ? "Day" : "Days"}`;
+    }
+    return "Date";
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount)
+      return `${guestCount} ${guestCount === "1" ? "Guest" : "Guests"}`;
+    return "Add guests";
+  }, [guestCount]);
+
   return (
     <div
       className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
       onClick={searchodal.onOpen}
     >
       <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6">Search</div>
+        <div className="text-sm font-semibold px-6">{locationLabel}</div>
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-          Date
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             {/* <BiSearch size={14} /> */}
             <ImSearch size={14} />
